89f8e77727d30be72fb19b2064f1610a46e36294
Federico Marra <marrafederico@proton.me>
2025-11-23 12:55:46 +0100

    add documentation

--- AuthLibrary/Repository/ICustomerSecurityRepository.cs
+++ AuthLibrary/Repository/ICustomerSecurityRepository.cs
@@ -2,15 +2,54 @@
 
 namespace AuthLibrary.Repository
 {
+    /// <summary>
+    /// Interface defining security and credential management operations for customers.
+    /// </summary>
     public interface ICustomerSecurityRepository
     {
-        public Task<bool> InsertCustomerSecurityAsync(UserRegisterRequest userData, int customerID);
-        public Task<bool> UpdatePasswordAsync(int customerId, string newPassword);
-        public Task<UserSecurityData?> GetCustomerSecurityByEmailAsync(string email);
-        public Task<bool> DeleteCustomerSecurityAsync(int customerID);
-        public Task RollbackCustomerSecurity(UserSecurityData userData);
+        /// <summary>
+        /// Inserts security data for a new customer into the system.
+        /// </summary>
+        /// <param name="userData">Object containing the user's registration information.</param>
+        /// <param name="customerID">ID of the customer associated with the security data.</param>
+        /// <returns>
+        /// Returns <c>true</c> if the insertion was successful; otherwise, <c>false</c>.
+        /// </returns>
+        Task<bool> InsertCustomerSecurityAsync(UserRegisterRequest userData, int customerID);
 
+        /// <summary>
+        /// Updates the password of an existing customer.
+        /// </summary>
+        /// <param name="customerId">ID of the customer whose password should be updated.</param>
+        /// <param name="newPassword">The new password to set.</param>
+        /// <returns>
+        /// Returns <c>true</c> if the update was successful; otherwise, <c>false</c>.
+        /// </returns>
+        Task<bool> UpdatePasswordAsync(int customerId, string newPassword);
 
+        /// <summary>
+        /// Retrieves security data of a customer based on their email.
+        /// </summary>
+        /// <param name="email">Email of the customer to search for.</param>
+        /// <returns>
+        /// Returns a <see cref="UserSecurityData"/> object if the customer is found; otherwise, <c>null</c>.
+        /// </returns>
+        Task<UserSecurityData?> GetCustomerSecurityByEmailAsync(string email);
 
+        /// <summary>
+        /// Deletes the security data of an existing customer.
+        /// </summary>
+        /// <param name="customerID">ID of the customer to delete.</param>
+        /// <returns>
+        /// Returns <c>true</c> if the deletion was successful; otherwise, <c>false</c>.
+        /// </returns>
+        Task<bool> DeleteCustomerSecurityAsync(int customerID);
+
+        /// <summary>
+        /// Rolls back the security data of a customer in case of an error
+        /// or incomplete operations.
+        /// </summary>
+        /// <param name="userData">Object containing the security data to restore.</param>
+        Task RollbackCustomerSecurity(UserSecurityData userData);
     }
 }
